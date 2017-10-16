438b45a391542588fcac416e8beaefa7ba190c55
holmanhub <holman.eric85@gmail.com>
2017-10-16 17:37:22 -0500

    added events.js

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,7 +5,8 @@ var outputDiv = $('#explosives');
 const domString = (explosives) => {
     let domStrang = "";
         domStrang += `<div>`;
-        domStrang +=`<h1>${explosives.type}</h1>`;
+        domStrang +=`<h1>${explosives.name}</h1>`;
+        domStrang +=`<h4>${explosives.description}</h4>`;
         domStrang +=`</div>`;
     printToDom(domStrang);
 };
